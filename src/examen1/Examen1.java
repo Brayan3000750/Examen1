194a458baa143c6ba387fc0e47f6677bc3f84155
BrayanX750 <brayan_pinel@unitec.edu>
2025-02-21 16:40:26 -0600

    aaaaa

--- src/examen1/Examen1.java
+++ src/examen1/Examen1.java
@@ -10,7 +10,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JComboBox; 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,7 +30,7 @@ private JFrame frame;
     private JComboBox<String> accountTypeComboBox;
     private JTextArea outputTextArea;
 
-    public SocialMediaGUI() {
+    public Examen1() {
         frame = new JFrame("UberSocial");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
